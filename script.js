820e96d3d794d2c89f7abc8f3b197e9feb51f6df
Cauan stipp <cauan.stipp@escola.pr.gov.br>
2025-11-11 12:16:39 +0000

    .

--- script.js
+++ script.js
@@ -5,9 +5,8 @@ janela.addEventListener("mouseout",fechar);
 
 function abrir(){
     janela.src = "img2.jpeg";
-    document.body.style.background = "#ffeb3b22"
-}
+    
 function fechar(){
     janela.src = "img.jpeg";
-    document.body.style.background = "#222"
+}
 }
