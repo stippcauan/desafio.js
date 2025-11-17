89145e334d6f07a28ca773dc65ba5274a753595a
Cauan stipp <cauan.stipp@escola.pr.gov.br>
2025-11-17 14:42:41 +0000

    finalizado

--- script.js
+++ script.js
@@ -1,12 +1,19 @@
 const janela = document.getElementById("janela");
 const luz = document.getElementById("luz")
 
-
 function abrir(){
-    janela.src = "img2.png";
+    janela.src = "janela.a.png";
       document.body.style.background = "#ffeb3b22";
-}   
+}
 function fechar(){
-    janela.src = "img.jpeg";
+    janela.src = "janela.f.png";
       document.body.style.background = " #000000"
+}
+function fechar_cortina(){
+    janela.src = "cortina.f.png";
+      document.body.style.background = " #000000"
+}
+function abrir_cortina(){
+    janela.src = "janela.a.png";
+     document.body.style.background = "#ffeb3b22";
 }
